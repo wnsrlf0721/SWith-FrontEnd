22b6b2fa3a30e27252084a4f61067a9bffb6a29e
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2021-11-18 00:03:35 +0900

    feat: 통계페이지 그래프 UI&기능 완성

--- frontend/src/components/main/MyPage.js
+++ frontend/src/components/main/MyPage.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import Container from "react-bootstrap/Container";
 import BtnPrev from "../../images/Prev.svg";
 import BtnNext from "../../images/Next.svg";
 import studyImage from "../../images/studyImage.jpg";
 import axios from "../../api/defaultaxios";
 import Progress from "../plan/Progress";
 import moment from "moment";
-import './StudyCard.css'
-import './MyPage.css'
+import "./StudyCard.css";
+import "./MyPage.css";
 import StudyCard from "./StudyCard";
 
 const MyPage = () => {
@@ -21,12 +19,12 @@ const MyPage = () => {
     count: 0,
   });
 
-  const hashtags=[{hashtag:'hashtag1'}]
-  
+  const hashtags = [{ hashtag: "hashtag1" }];
+
   //스터디 히스토리 추가
-  const studyBox=()=>{
+  const studyBox = () => {
     return (
-      <div className= "LinkContainer">
+      <div className="LinkContainer">
         <StudyCard
           title={"스터디기록1"}
           imgUrl={studyImage}
@@ -35,66 +33,92 @@ const MyPage = () => {
           nickName={NickName}
         ></StudyCard>
       </div>
-      
     );
-  }
-  const LoginStudy =(isLogined)=>{
-    if(isLogined){
+  };
+  const LoginStudy = (isLogined) => {
+    if (isLogined) {
       return (
         <>
           {studyBox()}
           {studyBox()}
         </>
-      )
+      );
     }
-  }
-  const LoginStudyTime =(isLogined)=>{
-    if(isLogined){
+  };
+  const LoginStudyTime = (isLogined) => {
+    if (isLogined) {
       return (
-      <div className= "StudyTimeBox">
-        <div className = "ColumnWrap" style = {{margin:"30px 40px",height: "100%",width:"80%"}}>
-          <div className= "ColumnWrap">
-          <div className= "TextBox" style={{ fontWeight: "bold", fontSize: "15px" }}>
-            오늘 공부한 시간
-          </div>
-          <div className= "TextBox" >{`${todaystudy.hour
-            .toString()
-            .padStart(2, "0")}:${todaystudy.minute
-            .toString()
-            .padStart(2, "0")}:${todaystudy.second
-            .toString()
-            .padStart(2, "0")}`}</div>
-          </div>
-          <div className= "Line"></div>
-          <div className= "ColumnWrap">
-            <div className= "TextBox" style={{ fontWeight: "bold", fontSize: "15px" }}>
-              오늘의 할일 달성률
+        <div className="StudyTimeBox">
+          <div
+            className="ColumnWrap"
+            style={{ margin: "30px 40px", height: "100%", width: "80%" }}
+          >
+            <div className="ColumnWrap">
+              <div
+                className="TextBox"
+                style={{ fontWeight: "bold", fontSize: "15px" }}
+              >
+                오늘 공부한 시간
+              </div>
+              <div className="TextBox">{`${todaystudy.hour
+                .toString()
+                .padStart(2, "0")}:${todaystudy.minute
+                .toString()
+                .padStart(2, "0")}:${todaystudy.second
+                .toString()
+                .padStart(2, "0")}`}</div>
             </div>
-            <div style={{ marginTop: "15px" ,alignItems: "center"}}>
-              <Progress
-                done={
-                  todaystudy.count === 0
-                    ? 0
-                    : Math.round(
-                        Number(todaystudy.complete / todaystudy.count) * 100
-                      )
-                }
-              />
+            <div className="Line"></div>
+            <div className="ColumnWrap">
+              <div
+                className="TextBox"
+                style={{ fontWeight: "bold", fontSize: "15px" }}
+              >
+                오늘의 할일 달성률
+              </div>
+              <div style={{ marginTop: "15px", alignItems: "center" }}>
+                <Progress
+                  done={
+                    todaystudy.count === 0
+                      ? 0
+                      : Math.round(
+                          Number(todaystudy.complete / todaystudy.count) * 100
+                        )
+                  }
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      )
-    }else{
+      );
+    } else {
       return (
-        <div className= "StudyTimeBox" style={{justifyContent: "center",flexDirection:"row"}}>
-          <div className= "studyText" style= {{color:"#454648", margin:"3px"}}>나의 학습 시간을 보고 싶다면</div>
-          <div className= "studyText" style= {{color:"#ef8585", margin:"3px"}}>로그인</div>
-          <div className= "studyText" style= {{color:"#454648", margin:"3px"}}>하세요!</div>
+        <div
+          className="StudyTimeBox"
+          style={{ justifyContent: "center", flexDirection: "row" }}
+        >
+          <div
+            className="studyText"
+            style={{ color: "#454648", margin: "3px" }}
+          >
+            나의 학습 시간을 보고 싶다면
+          </div>
+          <div
+            className="studyText"
+            style={{ color: "#ef8585", margin: "3px" }}
+          >
+            로그인
+          </div>
+          <div
+            className="studyText"
+            style={{ color: "#454648", margin: "3px" }}
+          >
+            하세요!
+          </div>
         </div>
-      )
+      );
     }
-  }
+  };
   const isLogined = window.sessionStorage.userInfo == null ? false : true;
   useEffect(() => {
     if (isLogined) {
@@ -136,8 +160,11 @@ const MyPage = () => {
         .then((response) => {
           const data = response.data.data;
           data.studyplanner_Tasks.map((task) => {
-            const plan_date = moment(task.startDate);
-            const Diff = Math.abs(Today.diff(plan_date, "days"));
+            const plan_date = moment(
+              moment(task.startDate).format("YYYY-MM-DD")
+            );
+            //console.log(Today, plan_date);
+            const Diff = Today.diff(plan_date, "days");
             if (Diff === 0) {
               //console.log(task.complete);
               setTodaystudy((prev) => ({
@@ -161,41 +188,49 @@ const MyPage = () => {
       //     console.log(error.toJSON());
       //   });
     }
-
   }, []);
 
   return (
-    <div className= "MainContainer">
-      <div className= "ContentsContainer">
-        <div className= "StudyHistoryWrap">
-          <div className= "StudyHistoryHeader">
-            <div className= "TextBox">내 스터디</div>
+    <div className="MainContainer">
+      <div className="ContentsContainer">
+        <div className="StudyHistoryWrap">
+          <div className="StudyHistoryHeader">
+            <div className="TextBox">내 스터디</div>
             <div>
               <img
-                style={{ height: "auto", width: "30px" ,cursor:"pointer"}}
+                style={{ height: "auto", width: "30px", cursor: "pointer" }}
                 src={BtnPrev}
                 alt="BtnPrev"
               />
               <img
-                style={{ height: "auto", width: "30px" ,cursor:"pointer"}}
+                style={{ height: "auto", width: "30px", cursor: "pointer" }}
                 src={BtnNext}
                 alt="BtnNext"
               />
             </div>
           </div>
-          <div className= "cardWrap">
+          <div className="cardWrap">
             {LoginStudy(isLogined)}
-            <div className= "LinkContainer" 
-              style= {{cursor: 'default',backgroundColor: "#454648",  display: "flex",alignItems: "center"}}>
-              <div className= "studyText">내가 만든 스터디 또는 초대 받은 스터디가 등록됩니다</div>
+            <div
+              className="LinkContainer"
+              style={{
+                cursor: "default",
+                backgroundColor: "#454648",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <div className="studyText">
+                내가 만든 스터디 또는 초대 받은 스터디가 등록됩니다
+              </div>
             </div>
           </div>
         </div>
-        <div className= "StudyTimeWrap">
-            <div className= "StudyTimeHeader">
-              <div className= "TextBox">학습 시간</div>
-            </div>
-            {LoginStudyTime(isLogined)}
+        <div className="StudyTimeWrap">
+          <div className="StudyTimeHeader">
+            <div className="TextBox">학습 시간</div>
+          </div>
+          {LoginStudyTime(isLogined)}
         </div>
       </div>
     </div>
