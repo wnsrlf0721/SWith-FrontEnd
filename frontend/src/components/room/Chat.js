bbf0ededcc5b57e7da77b6f6de7f61a2156318ba
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2021-11-21 12:34:52 +0900

    chore: git merge 수정 및 chat 엔터로 입력가능하게 수정

--- frontend/src/components/room/Chat.js
+++ frontend/src/components/room/Chat.js
@@ -1,69 +1,74 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { ChatMessage } from "./ChatMessage";
-import socket from "./utils/Socket"
-import './Chat.css';
+import socket from "./utils/Socket";
+import "./Chat.css";
 export const Chat = ({ userNickName }) => {
-    const [content, setContent] = useState("");
-    const [messages, setMessages] = useState([]);
- 
- 
+  const [content, setContent] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        socket.on('chatting', (data) => {
-            console.log(data);
-            setMessages((old) => [...old, data]);
-        });
-    }, []);
+  useEffect(() => {
+    socket.on("chatting", (data) => {
+      console.log(data);
+      setMessages((old) => [...old, data]);
+    });
+  }, []);
 
-    const onChangeContent = (e) => {
-        setContent(e.target.value);
-    };
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
 
-    const sendMessage = () => {
-        setMessages((old) => [...old, {
-            name: userNickName,
-            messageSendID: socket.id,
-            content: content,
-            date: new Date(),
-        }]);
+  const sendMessage = useCallback(
+    (e) => {
+      e.preventDefault();
+      setMessages((old) => [
+        ...old,
+        {
+          name: userNickName,
+          messageSendID: socket.id,
+          content: content,
+          date: new Date(),
+        },
+      ]);
 
-        console.log(content);
-        socket.emit("chatting", {
-            name: userNickName,
-            messageSendID: socket.id,
-            content: content,
-            date: new Date(),
-        });
-        setContent("");
-    };
+      console.log(content);
+      socket.emit("chatting", {
+        name: userNickName,
+        messageSendID: socket.id,
+        content: content,
+        date: new Date(),
+      });
+      setContent("");
+    },
+    [content]
+  );
 
-    
+  return (
+    <>
+      <div className="chatContainer">
+        <div className="TitleHeader">
+          <div className="Title">채팅</div>
+        </div>
+        <div id="chat">
+          <div className="ChatWrap">
+            {messages.map((message, index) => {
+              return (
+                <ChatMessage key={index} chat_message={message}></ChatMessage>
+              );
+            })}
+          </div>
 
-    return (
-        <>
-            <div className   = "chatContainer">
-                <div className="TitleHeader">
-                    <div className="Title">채팅</div>
-                </div>
-                <div id="chat" >
-                    <div className="ChatWrap">
-                        {
-                            messages.map((message, index) => {
-                                return <ChatMessage key={index} chat_message={message} ></ChatMessage>
-                                
-                            })
-                            
-                        }
-                    </div>
-                    
-                    <div className="InputBox">
-                        <input className="Input" onChange={onChangeContent} value={content} />
-                        <button className="Button" onClick={sendMessage}>전송</button>
-                    </div>
-                    
-                </div>
-            </div>
-            
-        </>
-    );
+          <div className="InputBox">
+            <form onSubmit={sendMessage}>
+              <input
+                className="Input"
+                onChange={onChangeContent}
+                value={content}
+              />
+              <button className="Button">전송</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
