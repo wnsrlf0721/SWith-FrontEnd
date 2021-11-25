d220c501eee2e6e249882e5e4633fb8999e56eaf
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2021-11-25 18:30:36 +0900

    feat: 커뮤니티 게시글 확인페이지 UI구성

--- frontend/src/components/comm/index.js
+++ frontend/src/components/comm/index.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 import Topbar from "../topbar";
-function index() {
+import post from "./post";
+
+const baseUrl = "/comm/";
+const Container = styled.div`
+  margin-top: 70px;
+  display: grid;
+  width: 70%;
+  height: 100%;
+  margin: 0 auto;
+  border: solid 1px #e4e6eb;
+  grid-template-columns: 1fr 3fr;
+`;
+
+const Index = () => {
   return (
     <>
       <Topbar />
-      <div style={{ position: "relative", marginTop: "64px" }}>
-        <h1>커뮤니티 페이지</h1>
+      <div style={{ marginTop: "64px", height: "100%" }}>
+        <Container>
+          <div>글 제목</div>
+          <BrowserRouter>
+            <Switch>
+              {/* <Route exact path={baseUrl} component={view} />
+            <Route path="" component={viewOtherUser} />
+            <Route path= component={edit} /> */}
+              <Route path={baseUrl + "post"} component={post} />
+            </Switch>
+          </BrowserRouter>
+        </Container>
       </div>
     </>
   );
-}
+};
 
-export default index;
+export default Index;
